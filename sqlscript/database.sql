2e6e357db6fcf8061f92ff8e2dac2fcb9c563246
Franco Nadal <fnadal01@gmail.com>
2025-05-27 11:19:37 -0300

    update del script sql
    agrego el campo token

--- sqlscript/database.sql
+++ sqlscript/database.sql
@@ -2,17 +2,18 @@ CREATE DATABASE questionmark;
 USE questionmark;
 
 CREATE TABLE Usuario (
-                          id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-                          nombre_completo VARCHAR(255) NOT NULL,
-                          ano_nacimiento INT NOT NULL,
-                          sexo ENUM('M', 'F', 'N') NOT NULL,
-                          pais VARCHAR(100),
-                          ciudad VARCHAR(100),
-                          email VARCHAR(255) NOT NULL UNIQUE,
-                          contrasenia VARCHAR(255) NOT NULL,
-                          nombre_usuario VARCHAR(50) NOT NULL UNIQUE,
-                          foto_perfil VARCHAR(255),
-                          rol ENUM('usuario', 'editor', 'administrador') DEFAULT 'usuario',
-                          cuenta_validada BOOLEAN DEFAULT FALSE,
-                          fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP
+                         id_usuario INT PRIMARY KEY AUTO_INCREMENT,                   -- 1
+                         nombre_completo VARCHAR(255) NOT NULL,                      -- 2
+                         ano_nacimiento INT NOT NULL,                                -- 3
+                         sexo ENUM('M', 'F', 'N') NOT NULL,                         -- 4
+                         pais VARCHAR(100),                                          -- 5
+                         ciudad VARCHAR(100),                                        -- 6
+                         email VARCHAR(255) NOT NULL UNIQUE,                        -- 7
+                         contrasenia VARCHAR(255) NOT NULL,                         -- 8
+                         nombre_usuario VARCHAR(50) NOT NULL UNIQUE,                -- 9
+                         foto_perfil VARCHAR(255),                                  -- 10
+                         rol ENUM('usuario', 'editor', 'administrador') DEFAULT 'usuario', -- 11
+                         cuenta_validada BOOLEAN DEFAULT FALSE,                     -- 12
+                         fecha_registro DATETIME DEFAULT CURRENT_TIMESTAMP,         -- 13 (movido aquí)
+                         token_validacion VARCHAR(255)                               -- 14 (ahora el último)
 );

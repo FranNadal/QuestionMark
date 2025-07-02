65e6b5cddf7cbce4de9f91c7654818a7e95cf3ef
Franco Nadal <fnadal01@gmail.com>
2025-07-02 14:50:55 -0300

    trabajo FINALIZADO

--- controller/HomeController.php
+++ controller/HomeController.php
@@ -13,12 +13,21 @@ class HomeController
 
     public function view()
     {
+        $idUsuario = $_SESSION['id_usuario'] ?? null;
 
         //es el mensaje de exito de la pregunta sugerida
         $mensaje = $_SESSION['mensaje_exito'] ?? null;
         unset($_SESSION['mensaje_exito']);
+        $nombre = $this->model->obtenerNombreUsuario($idUsuario);
+        $ranking = $this->model->obtenerRankingUsuario($idUsuario);
+        $partidas = $this->model->obtenerHistorialPartidas($idUsuario);
+
         $this->view->render("home", [
             'mensaje_exito' => $mensaje,
+            'nombre_usuario' => $nombre,
+            'posicion_ranking' => $ranking['posicion'],
+            'puntaje' => $ranking['puntaje'],
+            'partidas' => $partidas
         ]);
     }
 
--- model/HomeModel.php
+++ model/HomeModel.php
@@ -7,5 +7,48 @@ class HomeModel
     public function __construct($database){
         $this->database = $database;
     }
+    public function obtenerNombreUsuario($idUsuario)
+    {
+        $resultado = $this->database->fetchOne(
+            "SELECT nombre_usuario FROM usuario WHERE id_usuario = ?",
+            [$idUsuario]
+        );
+        return $resultado['nombre_usuario'] ?? 'Usuario';
+    }
+
+    public function obtenerRankingUsuario($idUsuario)
+    {
+        $sql = "
+        SELECT posicion, puntaje FROM (
+            SELECT id_usuario,
+                   MAX(puntaje) AS puntaje,
+                   RANK() OVER (ORDER BY MAX(puntaje) DESC) AS posicion
+            FROM partida
+            GROUP BY id_usuario
+        ) AS ranking
+        WHERE id_usuario = ?
+    ";
+
+        $resultado = $this->database->fetchOne($sql, [$idUsuario]);
+
+        return $resultado ?: ['posicion' => 'N/A', 'puntaje' => 0];
+    }
+
+
+    public function obtenerHistorialPartidas($idUsuario)
+    {
+        return $this->database->fetchAll(
+            "SELECT 
+            puntaje,
+            estado AS resultado,
+            DATE_FORMAT(fecha_inicio, '%e/%c/%Y') AS fecha_formateada
+         FROM partida
+         WHERE id_usuario = ?
+         ORDER BY fecha_inicio DESC
+         LIMIT 5",
+            [$idUsuario]
+        );
+    }
+
 
 }
